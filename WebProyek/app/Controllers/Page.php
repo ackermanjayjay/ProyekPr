593559ea768eab73d9f6968fdf0993c344c5e4ce
ackermanjayjay <reza23198@gmail.com>
2021-05-20 14:37:38 +0700

    Baru menambahkan view dan Controller

---
+++ WebProyek/app/Controllers/Page.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controllers;
+
+class Page extends BaseController
+{
+	public function index()
+	{
+		echo "tangkap page";
+	}
+    public function contact()
+    {
+        echo "tangkap contak";
+    }
+    public function faqs()
+    {
+        echo "tangkap faqs";
+    }
+
+public function about()
+{
+    echo "tangkp about";
+}
+}
